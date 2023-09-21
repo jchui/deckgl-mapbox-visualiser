8d4398e31c384ed25df812d4463bb0da1188442a
Jeremy Chui <jeremy@jchui.me>
2023-09-21 15:09:50 +0100

    Code cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,26 +4,24 @@ import MapCanvas from "./Map";
 import { AiFillDatabase } from "react-icons/ai";
 
 const Home = () => {
-  const [selectedLayers, setSelectedLayers] = useState([]) as any;
-  const [layerData, setLayerData] = useState(layers) as any;
-  const toggleLayer = (e: any) => {
-    const newLayer = layerData.map((layer: any) => {
-      if (layer.id === e.id) {
-        return {
-          ...layer,
-          isShown: !layer.isShown,
-        };
-      } else {
-        return layer;
-      }
-    });
-    setLayerData(newLayer);
-  };
+  const [selectedLayers, setSelectedLayers] = useState<any>([]);
+  const [layerData, setLayerData] = useState<any>(layers);
   const [position, setPosition] = useState({
     latitude: 0,
     longitude: 0,
     zoom: 0,
   });
+
+  const toggleLayer = (e: any) => {
+    const newLayer = layerData.map((layer: any) => {
+      if (layer.id === e.id) {
+        return { ...layer, isShown: !layer.isShown };
+      }
+      return layer;
+    });
+    setLayerData(newLayer);
+  };
+
   const handleActiveLayers = (e: any) => {
     setPosition({
       ...position,
@@ -31,33 +29,23 @@ const Home = () => {
       longitude: parseFloat(e.longitude),
       zoom: parseFloat(e.zoom),
     });
-    setSelectedLayers({
-      name: e.name,
-    });
+
+    setSelectedLayers({ name: e.name });
 
     setLayerData((prev: any) => {
       return prev.map((layer: any) => {
         if (layer.id === e.id) {
-          return {
-            ...layer,
-            isShown: !layer.isShown,
-          };
-        } else {
-          return {
-            ...layer,
-          };
+          return { ...layer, isShown: !layer.isShown };
         }
+        return layer;
       });
     });
   };
-  const activeLayers = layerData.filter((e: any) => e.isShown === true);
-  return (
-    <div
-      style={{
-        overflow: "hidden",
-      }}
-    >
 
+  const activeLayers = layerData.filter((e: any) => e.isShown);
+
+  return (
+    <div style={{ overflow: "hidden" }}>
       <div className="logo">
         <h1>Healthcare Data Visualisation</h1>
         <p>Description goes here</p>
@@ -67,55 +55,47 @@ const Home = () => {
         <div className="menu-header">
           <p className="title">Healthcare Datasets</p>
           <p className="description">
-          Visualize and explore UK healthcare data on an interactive map. 
+            Visualize and explore UK healthcare data on an interactive map.
           </p>
         </div>
 
         <div className="datasets-list">
-          {layerData?.map((e: any, index: any) => (
-              <div
-                key={index}
-                className="dataset"
-              >
-                <a onClick={() => handleActiveLayers(e)}>
-                  <div className="icon"><AiFillDatabase /> </div>
-                  <h4>{e.name}</h4>
-                </a>
-                <input
-                  id="toggle"
-                  checked={
-                    layerData.find((layer: any) => layer.id === e.id).isShown
-                  }
-                  onChange={() => toggleLayer(e)}
-                  type="checkbox"
-                />
-                <span className="checkbox-select-toggle">
-                  <p className="description">{e.description}</p>
-                  <p className="source">{e.source}</p>
-                  <span className="active">Active</span>
-                  <span className="layer-type">Type: {e.layer}</span>
-                </span>
-              </div>
-            ))}
+          {layerData?.map((e: any, index: number) => (
+            <div key={index} className="dataset">
+              <a onClick={() => handleActiveLayers(e)}>
+                <div className="icon">
+                  <AiFillDatabase />
+                </div>
+                <h4>{e.name}</h4>
+              </a>
+              <input
+                id="toggle"
+                checked={layerData.find((layer: any) => layer.id === e.id).isShown}
+                onChange={() => toggleLayer(e)}
+                type="checkbox"
+              />
+              <span className="checkbox-select-toggle">
+                <p className="description">{e.description}</p>
+                <p className="source">{e.source}</p>
+                <span className="active">Active</span>
+                <span className="layer-type">Type: {e.layer}</span>
+              </span>
+            </div>
+          ))}
         </div>
 
         <div className="menu-footer">
           <p>{layerData.length} datasets ready for visualisation.</p>
         </div>
-
       </div>
 
       <div className="footer">
-        <p>Built in ReactJS with DeckGL and MapBox. Copyright &copy; 2023.</p>
+        <p>
+          Built in ReactJS with DeckGL and MapBox. Copyright &copy; 2023.
+        </p>
       </div>
 
-      <div
-        style={{
-          height: "100vh",
-          width: "100vw",
-          position: "relative",
-        }}
-      >
+      <div style={{ height: "100vh", width: "100vw", position: "relative" }}>
         <MapCanvas position={position} activeLayers={activeLayers} />
       </div>
     </div>
--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,76 +1,78 @@
-// @ts-ignore
 import DeckGL, {
   ScatterplotLayer,
   HexagonLayer,
   GreatCircleLayer,
   ArcLayer,
-  //   @ts-ignore
 } from "deck.gl";
 import { Map } from "react-map-gl";
 import { useMemo, useState } from "react";
 import Loader from "../components/Loader";
-const MAP_STYLE =
-  "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
-// @ts-ignore
 
+const MAP_STYLE = "https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json";
 const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+
 const MapCanvas = ({ position, activeLayers }: any) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const layers = useMemo(() => {
     if (!activeLayers) return [];
+
     return activeLayers.map((e: any) => {
-      if (e.layer === "scatterplot") {
-        const scatterlayer = new ScatterplotLayer({
-          id: e.name,
-          data: e.json,
-          radiusScale: 10,
-          radiusMinPixels: 1,
-          getPosition: (d: any) => [d[0], d[1], 0],
-          getFillColor: e.scatterplotColor,
-          extruded: true, // Enable extrusion
-          pickable: true,
-        });
-        return scatterlayer;
-      }
-      if (e.layer === "hexagon") {
-        const hexagonLayer = new HexagonLayer({
-          id: e.name,
-          data: e.json,
-          elevationRange: [0, 3000],
-          elevationScale: e.json && e.json.length ? 50 : 0,
-          extruded: true, // Enable extrusion
-          getColor: [255, 0, 100],
-          getElevation: (d: any) => 100,
-          getPosition: (d: any) => [d.lng, d.lat],
-          pickable: true,
-        });
-        return hexagonLayer;
-      } else if (e.layer === "greatcircle") {
-        const greatCircleLayer = new GreatCircleLayer({
-          id: e.name,
-          data: e.json,
-          getSourcePosition: (d: any) => d.from.coordinates,
-          getTargetPosition: (d: any) => d.to.coordinates,
-          getSourceColor: [64, 255, 0],
-          getTargetColor: [0, 128, 200],
-          widthMinPixels: e.width,
-          extruded: true, // Enable extrusion
-          getElevation: (d: any) => 1000,
-          pickable: true,
-        });
-        return greatCircleLayer;
-      } else if (e.layer === "arc") {
-        const arcLayer = new ArcLayer({
-          id: e.name,
-          data: e.json,
-          getSourcePosition: (d: any) => d.from.coordinates,
-          getTargetPosition: (d: any) => d.to.coordinates,
-          getSourceColor: [64, 255, 0],
-          getTargetColor: [0, 128, 200],
-          getWidth: e.width,
-          extruded: true, // Enable extrusion
-          pickable: true,
-        });
-        return arcLayer;
+      switch (e.layer) {
+        case "scatterplot":
+          return new ScatterplotLayer({
+            id: e.name,
+            data: e.json,
+            radiusScale: 10,
+            radiusMinPixels: 1,
+            getPosition: (d: any) => [d[0], d[1], 0],
+            getFillColor: e.scatterplotColor,
+            extruded: true,
+            pickable: true,
+          });
+
+        case "hexagon":
+          return new HexagonLayer({
+            id: e.name,
+            data: e.json,
+            elevationRange: [0, 3000],
+            elevationScale: e.json && e.json.length ? 50 : 0,
+            extruded: true,
+            getColor: [255, 0, 100],
+            getElevation: (d: any) => 100,
+            getPosition: (d: any) => [d.lng, d.lat],
+            pickable: true,
+          });
+
+        case "greatcircle":
+          return new GreatCircleLayer({
+            id: e.name,
+            data: e.json,
+            getSourcePosition: (d: any) => d.from.coordinates,
+            getTargetPosition: (d: any) => d.to.coordinates,
+            getSourceColor: [64, 255, 0],
+            getTargetColor: [0, 128, 200],
+            widthMinPixels: e.greatCircleWidth,
+            extruded: true,
+            getElevation: (d: any) => 1000,
+            pickable: true,
+          });
+
+        case "arc":
+          return new ArcLayer({
+            id: e.name,
+            data: e.json,
+            getSourcePosition: (d: any) => d.from.coordinates,
+            getTargetPosition: (d: any) => d.to.coordinates,
+            getSourceColor: [64, 255, 0],
+            getTargetColor: [0, 128, 200],
+            getWidth: e.width,
+            extruded: true,
+            pickable: true,
+          });
+
+        default:
+          return null;
       }
     });
   }, [activeLayers]);
@@ -81,18 +83,11 @@ const MapCanvas = ({ position, activeLayers }: any) => {
     zoom: position.zoom,
     maxZoom: 16,
   };
-  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <>
       {isLoading && (
-        <div
-          style={{
-            position: "absolute",
-            zIndex: 1000,
-            left: "50%",
-            top: "50%",
-          }}
-        >
+        <div style={{ position: "absolute", zIndex: 1000, left: "50%", top: "50%" }}>
           <Loader />
         </div>
       )}
@@ -105,12 +100,12 @@ const MapCanvas = ({ position, activeLayers }: any) => {
           if (object.layer && object.coordinate) {
             return {
               html: `
-            <div>
-              <h2>${object?.layer?.id}</h2>
-              <p>X: ${object?.coordinate[0]}</p>
-              <p>Y: ${object?.coordinate[1]}</p>
-            </div>
-            `,
+                <div>
+                  <h2>${object?.layer?.id}</h2>
+                  <p>X: ${object?.coordinate[0]}</p>
+                  <p>Y: ${object?.coordinate[1]}</p>
+                </div>
+                `,
             };
           }
         }}
